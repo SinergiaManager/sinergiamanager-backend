1418abe7b2e26c367b66087a1baaf5a08f254b5c
Federico Balducci <ott8v@proton.me>
2024-09-20 21:04:58 +0000

    feat: add test route

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "github.com/kataras/iris/v12"
 
+func main() {
+	app := iris.New()
+
+	type PingResponse struct {
+		Message string `json:"message"`
+	}
+
+	app.Get("/ping", func(ctx iris.Context) {
+		res := PingResponse{
+			Message: "pong",
+		}
+
+		ctx.JSON(res)
+	})
+
+	app.Listen(":8080")
 }
