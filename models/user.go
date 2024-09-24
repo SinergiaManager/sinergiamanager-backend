e3695fa758697314aa7f44f569a000dac4a4eb6a
Federico Balducci <ott8vv@gmail.com>
2024-09-24 22:45:01 +0200

    Refactor(models/user.go): Update User struct fields

--- models/user.go
+++ models/user.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type User struct {
-	username string    `json:"username"`
-	name     string    `json:"name"`
-	surname  string    `json:"surname"`
-	email    string    `json:"email"`
-	insertAt time.Time `json:"insert_at"`
-	updateAt time.Time `json:"update_at"`
+	ID       string `bson:"_id"`
+	Username string
+	Name     string
+	Surname  string
+	Email    string
+	InsertAt time.Time
+	UpdateAt time.Time
 }
