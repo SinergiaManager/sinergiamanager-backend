f1b0978b8946461c8650fa3493b9966ef71df79b
Federico Balducci <ott8vv@gmail.com>
2024-10-14 15:32:54 +0200

    feat: Add GetWarehouseById, CreateWarehouse, and DeleteWarehouse endpoints

--- controllers/warehouses.go
+++ controllers/warehouses.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"time"
+
 	Config "github.com/SinergiaManager/sinergiamanager-backend/config"
 	Models "github.com/SinergiaManager/sinergiamanager-backend/models"
 	"github.com/kataras/iris/v12"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -42,4 +46,80 @@ func GetAllWarehouses(ctx iris.Context) {
 	ctx.StatusCode(iris.StatusOK)
 	ctx.JSON(iris.Map{"data": warehouses})
 
+}
+
+func GetWarehouseById(ctx iris.Context) {
+	warehouse := &Models.WarehouseDb{}
+	id := ctx.Params().Get("id")
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.JSON(iris.Map{"error": err.Error()})
+		return
+	}
+
+	err = Config.DB.Collection("warehouses").FindOne(ctx, bson.M{"_id": objID}).Decode(warehouse)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			ctx.StatusCode(iris.StatusNotFound)
+			ctx.JSON(iris.Map{"error": "Warehouse not found"})
+			return
+		} else {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.JSON(iris.Map{"error": "Faled to fect warehouse"})
+		}
+		return
+	}
+
+	ctx.JSON(iris.Map{"data": warehouse})
+}
+
+func CreateWarehouse(ctx iris.Context) {
+	var warehouse *Models.WarehouseIn
+	err := ctx.ReadBody(&warehouse)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.JSON(iris.Map{"error": err.Error()})
+		return
+	}
+	warehouse.InsertAt = time.Now().UTC()
+	warehouse.UpdateAt = time.Now().UTC()
+
+	_, err = Config.DB.Collection("warehouses").InsertOne(ctx, warehouse)
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.JSON(iris.Map{"error": err.Error()})
+		return
+	}
+
+	ctx.StatusCode(iris.StatusCreated)
+	ctx.JSON(iris.Map{"message": "Warehouse created successfully"})
+
+}
+
+func DeleteWarehouse(ctx iris.Context){
+	id := ctx.Params().Get("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.JSON(iris.Map{"error": "Invalid warehouse ID format"})
+		return
+	}
+	filter := bson.M{"_id": objectID}
+	result, err := Config.DB.Collection("warehouses").DeleteOne(ctx, filter)
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.JSON(iris.Map{"error": err.Error()})
+		return
+	}
+
+	if result.DeletedCount == 0 {
+		ctx.StatusCode(iris.StatusNotFound)
+		ctx.JSON(iris.Map{"error": "Warehouse not found"})
+		return
+	}
+
+	ctx.StatusCode(iris.StatusOK)
+	ctx.JSON(iris.Map{"message": "Warehouse deleted successfully"})
 }
